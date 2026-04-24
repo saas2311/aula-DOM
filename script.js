f13225a202b8c6376f5f47269dbe67a00dac4f18
saas2311 <sarahlopess1123@gmail.com>
2026-04-23 21:37:28 -0300

    ok

---
+++ script.js
@@ -0,0 +1,69 @@
+const livros = [
+  { id: 1, titulo: "Dom Casmurro", preco: 29.9 },
+  { id: 2, titulo: "Clean Code", preco: 49.9 },
+  { id: 3, titulo: "O Senhor dos Anéis", preco: 79.9 },
+  { id: 4, titulo: "O Alquimista", preco: 39.9 },
+];
+
+let carrinho = [];
+let total = 0;
+
+const listaLivrosHtml = document.getElementById("lista-livros");
+const itensCarrinhoHtml = document.getElementById("itens-carrinho");
+const valorTotalHtml = document.getElementById("valor-total");
+
+function carregarCatalogo() {
+  livros.forEach((livro) => {
+    const div = document.createElement("div");
+    div.classList.add("livro-card");
+    div.innerHTML = `
+        <h3>${livro.titulo}</h3>
+        <p>R$ ${livro.preco.toFixed(2)}</p>
+        <button onclick='adicionarAoCarrinho(${livro.id})'>Adicionar</button>
+
+      `;
+    listaLivrosHtml.appendChild(div);
+  });
+}
+
+function adicionarAoCarrinho(id) {
+  const livro = livros.find((l) => l.id === id);
+  carrinho.push(livro);
+  total += livro.preco;
+  atualizarInterfaceCarrinho();
+}
+
+function removerItem(index) {
+  total -= carrinho[index].preco;
+  carrinho.splice(index, 1);
+  if (carrinho.length === 0) total = 0;
+
+  atualizarInterfaceCarrinho();
+}
+
+function esvaziarCarrinho() {
+  carrinho = [];
+  total = 0;
+  atualizarInterfaceCarrinho();
+}
+
+function atualizarInterfaceCarrinho() {
+  itensCarrinhoHtml.innerHTML = "";
+  carrinho.forEach((item, index) => {
+    const li = document.createElement("li");
+    li.innerHTML = `${item.titulo} - R$ ${item.preco.toFixed(2)}`;
+    li.onclick = () => removerItem(index);
+    itensCarrinhoHtml.appendChild(li);
+  });
+  valorTotalHtml.textContent = total.toFixed(2);
+
+  if (total > 100) {
+    valorTotalHtml.style.color = "red";
+    valorTotalHtml.style.fontWeight = "bold";
+  } else {
+    valorTotalHtml.style.color = "black";
+    valorTotalHtml.style.fontWeight = "normal";
+  }
+}
+
+carregarCatalogo();
